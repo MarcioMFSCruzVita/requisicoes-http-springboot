fa53d423e831d2d961b77384cc8aa645c242f2d6
marciomfs <marcio.mendonca@cruzvita.com>
2022-09-06 13:31:43 -0300

    criando classe Application com spring

---
+++ requisicoes-http-spring/src/main/java/br/com/cruzvita/requisicoes/fixacao/FixacaoHttpApplication.java
@@ -0,0 +1,13 @@
+package br.com.cruzvita.requisicoes.fixacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixacaoHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixacaoHttpApplication.class, args);
+	}
+
+}
