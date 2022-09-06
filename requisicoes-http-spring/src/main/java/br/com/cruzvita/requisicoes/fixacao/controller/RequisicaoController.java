cae9184a4fc004c465e62b081b0a7cfda5b830a6
marciomfs <marcio.mendonca@cruzvita.com>
2022-09-06 17:04:08 -0300

    criando sistema de login com post e criptografando senha com biblioteca javax.crypto

--- requisicoes-http-spring/src/main/java/br/com/cruzvita/requisicoes/fixacao/controller/RequisicaoController.java
+++ requisicoes-http-spring/src/main/java/br/com/cruzvita/requisicoes/fixacao/controller/RequisicaoController.java
@@ -2,14 +2,68 @@ package br.com.cruzvita.requisicoes.fixacao.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import br.com.cruzvita.requisicoes.fixacao.dto.AutenticacaoDTO;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.EncryptedPrivateKeyInfo;
+import javax.crypto.spec.IvParameterSpec;
 
 @RestController
 @RequestMapping("/requisicao")
 public class RequisicaoController {
-	
+
+	static String IV = "AAAAAAAAAAAAAAAA";
+	static String textopuro = "teste texto 12345678\0\0\0";
+	static String chaveencriptacao = "0123456789abcdef";
+
 	@GetMapping("/soma")
 	public String requisicaoSoma(@RequestParam int n1, @RequestParam int n2) {
-		int resultado = n1+n2;
+		int resultado = n1 + n2;
 		return "Resultado da soma: " + resultado;
+	}
+
+	@PostMapping("/login")
+	public String atenticacao(@RequestBody AutenticacaoDTO login) throws Exception {
+		String senhacripto = null;
+		String senhaDecript = null;
+		String erro;
+		login.autenticado = false;
+
+		if (login.getEmail() != null && login.getSenha() != null && login.getSenha().length() >= 8
+				&& login.getEmail().contains("@")) {
+			login.autenticado = true;
+
+			login.setSenhaCriptografada(encrypt(login.getSenha(), chaveencriptacao));
+
+			senhaDecript = decrypt(login.getSenhaCriptografada(), chaveencriptacao);
+			login.setSenhaDecriptografada(senhaDecript);
+		} else {
+			login.autenticado = false;
+			
+		}
+
+		if (login.autenticado == true) {
+
+			return "Você está logado" + "\n" + login;
+
+		} else {
+			erro = (login.getSenha().length() < 8) ? "senha invalida, menor que 8" : "email invalido";
+			return "Dados errados." + "\n" + erro + "\n" + login;
+		}
+	}
+
+	public static byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+		Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		encripta.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		return encripta.doFinal(textopuro.getBytes("UTF-8"));
+	}
+
+	public static String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception {
+		Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		decripta.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		return new String(decripta.doFinal(textoencriptado), "UTF-8");
 	}
 }
--- requisicoes-http-spring/src/main/java/br/com/cruzvita/requisicoes/fixacao/dto/AutenticacaoDTO.java
+++ requisicoes-http-spring/src/main/java/br/com/cruzvita/requisicoes/fixacao/dto/AutenticacaoDTO.java
@@ -4,7 +4,21 @@ public class AutenticacaoDTO {
 	private String email;
 	private String senha;
 	public Boolean autenticado;
+	private byte[] senhaCriptografada;
+	private String senhaDecriptografada;
 	
+	public String getSenhaDecriptografada() {
+		return senhaDecriptografada;
+	}
+	public void setSenhaDecriptografada(String senhaDecriptografada) {
+		this.senhaDecriptografada = senhaDecriptografada;
+	}
+	public byte[] getSenhaCriptografada() {
+		return senhaCriptografada;
+	}
+	public void setSenhaCriptografada(byte[] senhaCriptografada) {
+		this.senhaCriptografada = senhaCriptografada;
+	}
 	public String getEmail() {
 		return email;
 	}
@@ -18,4 +32,12 @@ public class AutenticacaoDTO {
 		this.senha = senha;
 	}
 	
+	public String toString() {
+		if(autenticado == true) {
+			return "\nemail:" + email + "\n" + "senha:" + senhaCriptografada + "/n senha decript: " + senhaDecriptografada;
+		}else {
+			return "\nemail:" + email + "\n" + "senha:" + senha;
+		}
+		
+	}
 }
